0688114683d39b16bc86a79b5951987f4dc194bc
totoalpina <totoalpina@yahoo.com>
2022-10-21 16:19:55 +0300

    Added movies API, refactor index, login, footer

--- IMDb/assets/js/index.js
+++ IMDb/assets/js/index.js
@@ -1,41 +1,72 @@
-window.addEventListener('load', populateMovies)
+window.addEventListener('load', loadMovies)
 let moviesNR = [];
 
-function populateMovies() {
-    getData().then(data => {
-        moviesNR = data;
-        
-        let rowNR = document.querySelector('#movies-list-new-releases')
+function populateMovies(movies) {
+    let rowNR = document.querySelector('#movies-list-new-releases')
 
-        rowNR.innerHTML = '';
-        for (movie of moviesNR) {
-            rowNR.append(loadElement(movie.movieTitle, movie.URL, movie.movieDescription, movie.movieRating))
-        }
+    rowNR.innerHTML = '';
+    for (let movie of movies) {
+        rowNR.append(loadElement(movie.movie_title, movie.movie_image, movie.movie_trailer_url, movie.movie_description, movie.movie_rating))
+    }
+}
+
+function loadMovies() {
+    getMovies('http://127.0.0.1:3000/users')
+    populateMovies(moviesNR)
+
+}
+
+function getMovies(url) {
+    // let xhr = new XMLHttpRequest()
+
+    // xhr.open('GET', url, true)
+
+    // xhr.onload = function () {
+    //     // console.log(responseText);
+    //     data = this.response;
+    //     console.log(this.statusText);
+    //     if (this.status === 200) {
+
+    //         moviesNR = JSON.parse(data)
+    //         console.log(moviesNR);
+    //         populateMovies(moviesNR)
+
+    //     } else {
+    //         console.log('could not load')
+    //     }
+    // }
+
+    // xhr.send()
+
+    fetch(url)
+    .then(function(response) {
+        return response.text()
     })
+    .then(
+        (data) => {
+            moviesNR = JSON.parse(data)
+            console.log(moviesNR);
+            populateMovies(moviesNR) 
+        }
+    )
 }
 
-const getData = async () => {
-    const response = await fetch('assets/data.json')
-    moviesNR = await response.json()
-
-    return moviesNR
-}
-
-function loadElement(title, img, description, rating) {
+function loadElement(title, img, trailer, description, rating) {
 
     let cardNR = document.createElement('div')
     cardNR.className = 'col-md-6 col-lg-3'
     cardNR.innerHTML = '<div class="card bg bg-dark text-white">' +
-                            '<img src="' + img + '" alt="" class="card-img-top">' +
-                            '<div class="card-body">' +
-                                '<div class="bg bg-dark pb-2"> <span class="text-muted">IMDb rating : </span>' +
-                                    '<i class="bi bi-heart-fill rated"> - <span class="rated" id="rated-nr-span">' + rating + '</span></i>' +
-                                '</div>' +
-                                '<h6 class="card-title lead text-center" id="nr-movie-title">' + title + '</h6>' +
-                                '<p class="card-text text-muted text-truncate" id="nr-description">' + description +
-                                '</p>' +
-                            '</div>' +
-                        '</div> '
+        '<img src="' + img + '" alt="" class="card-img-top">' +
+        '<div class="card-body">' +
+        '<div class="bg bg-dark pb-2"> <span class="text-muted">IMDb rating : </span>' +
+        '<i class="bi bi-heart-fill rated"> - <span class="rated" id="rated-nr-span">' + rating + '</span></i>' +
+        '</div>' +
+        '<h6 class="card-title lead text-center" id="nr-movie-title">' + title + '</h6>' +
+        '<p class="card-text text-muted text-truncate" id="nr-description">' + description +
+        '</p>' +
+        '</div>' +
+        '<div class="card-footer"><a class="text-white" href="' + trailer + '" target="_blank" style="text-decoration: none;">Trailer</a> </div>' +
+        '</div> '
 
     return cardNR
 }
